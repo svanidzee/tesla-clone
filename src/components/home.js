f215577d1fadc9b3aa052b8050f97c18d543a12b
svanidzee <svanidzeee@gmail.com>
2022-04-20 15:54:49 +0100

    add home section and styled

--- src/components/home.js
+++ src/components/home.js
@@ -1,8 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Section from "./section";
 
 const Home = () => {
-  return <Container>Home</Container>;
+  return (
+    <Container>
+      <Section />
+    </Container>
+  );
 };
 
 export default Home;
---
+++ src/components/section.js
@@ -0,0 +1,59 @@
+import React from "react";
+import styled from "styled-components";
+
+const Section = () => {
+  return (
+    <Wrap>
+      <ItemText>
+        <h1>Model S</h1>
+        <p>Order Online for Touchless Delivery</p>
+      </ItemText>
+      <ButtonGroup>
+        <LeftButton>Custom Order</LeftButton>
+        <RightButton>Existing Invetory</RightButton>
+      </ButtonGroup>
+    </Wrap>
+  );
+};
+
+export default Section;
+
+const Wrap = styled.div`
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-image: url("/images/model-s.jpg");
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ItemText = styled.div`
+  padding-top: 15vh;
+  text-align: center;
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  margin-bottom: 30px;
+`;
+
+const LeftButton = styled.div`
+  background-color: rgba(23, 26, 32, 0.8);
+  height: 40px;
+  width: 256px;
+  color: white;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border-radius: 100px;
+  opacity: 0.85;
+  text-transform: uppercase;
+  font-size: 12px;
+  cursor: pointer;
+`;
+
+const RightButton = styled(LeftButton)``;
