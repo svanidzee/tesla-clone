f5ea6604035010f6810b0c53dfa442a4dae53dc6
svanidzee <svanidzeee@gmail.com>
2022-04-21 14:52:47 +0100

    df

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Header from "../components/header";
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <Fragment>
       <Header />
       {children}
-    </>
+    </Fragment>
   );
 };
 
